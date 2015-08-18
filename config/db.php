28812fff99959d12ab592bd71d585fbaec4f4e4b
chang <335383263@qq.com>
2015-08-18 22:48:27 +0800

    initrialized

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+
+    'dsn' => 'mysql:host=182.92.236.216;dbname=Ontee',
+    'username' => 'root',
+    'password' => '1qaz@WSX',
+    'charset' => 'utf8',
+];
