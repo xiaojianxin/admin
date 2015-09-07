57a8a8d90b5a9c9ea21361eb31b93a1be8b561a9
liuchang <lchangrwx@gmail.com>
2015-09-08 00:04:03 +0800

    picture uplioad chang

---
+++ controllers/UploadController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\models\UploadForm;
+use yii\web\UploadedFile;
+use app\models\Pictures;
+
+class UploadController extends Controller
+{
+	public function actionUpload()
+	{
+		$model = new UploadForm();
+		$pic = new Pictures();
+
+        if (Yii::$app->request->isPost) {
+            $model->file = UploadedFile::getInstance($model, 'file');
+
+            if ($model->validate()) {                
+                $model->file->saveAs('../pictures/' . $model->file->baseName . '.' . $model->file->extension);
+            	$pic->url = 'pictures/'.$model->file->baseName.'.'.$model->file->extension;
+            	$pic->save();
+            }
+        }
+
+        return $this->render('upload', ['model' => $model]);
+	}
+}
